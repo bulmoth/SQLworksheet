32b146c471025fd007114b67f694405dee75e75f
INTEL-PC\Intel <ji>
2022-07-28 10:41:23 +0900

    세미파일

---
+++ SEMI_SCRIPT.sql
@@ -0,0 +1,27 @@
+-- SEMI DDL
+
+--------------------------------
+-- MEMBER
+--------------------------------
+
+DROP TABLE MEMBER;
+CREATE TABLE MEMBER(
+    NO              NUMBER          PRIMARY KEY
+    , ID            VARCHAR2(30)    NOT NULL UNIQUE
+    , PWD           VARCHAR2(100)   NOT NULL
+    , NAME          VARCHAR2(15)    NOT NULL
+    , PHONE         CHAR(13)
+    , EMAIL         VARCHAR2(100)
+    , ADDR          VARCHAR2(100)
+    , INTEREST      VARCHAR2(100)
+    , ENROLL_DATE   TIMESTAMP       DEFAULT SYSDATE
+    , MODIFY_DATE   TIMESTAMP       DEFAULT SYSDATE
+    , STATUS        CHAR(1)         DEFAULT 'N' CHECK(STATUS IN ('Y','N')) 
+);
+
+DROP SEQUENCE SEQ_MEMBER_NO;
+CREATE SEQUENCE SEQ_MEMBER_NO NOCACHE NOCYCLE;
+
+INSERT INTO MEMBER(NO, ID, PWD, NAME, PHONE, EMAIL, ADDR, INTEREST) VALUES(SEQ_MEMBER_NO.NEXTVAL, 'USER00', '1234', '홍길동', '01012341234', 'abc@naver.com', '강남구 역삼동 테헤란로 1번길', '');
+
+COMMIT;
